ade78c12c92c032bed5b9a1e80a3a6eaec85e26f
Em Es En <stokogsten@gmail.com>
2016-10-28 13:18:22 +0200

    bspwm files only

--- herbstluftwm/tray.sh
+++
@@ -1,4 +0,0 @@
-trayer --edge top --align right --widthtype request --SetDockType true --heighttype pixel \
-       --height 10 --expand true --tint 0x222222 --transparent true \
-       --alpha 0 --margin 330
-       
